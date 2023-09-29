9af0da8be862fa2ee91fac4cf11be8277819ce4c
soundscapeofwar <145902285+soundscapeofwar@users.noreply.github.com>
2023-09-29 20:39:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,10 @@
-var playBtn = document.getElementById('play'),
-  resetBtn = document.getElementById('reset'),
-  hearbeat = document.getElementById('heartbeat')
-	audios = document.querySelectorAll('audio');
-console.log(audios);
-
-
-playBtn.addEventListener('mouseover', function() {
-[].forEach.call(audios, function(audio) {
-  // do whatever
-  audio.play();
-});
-}, false);
-
-playBtn.addEventListener('mouseleave', function() {
-  heartbeat.pause();
-  heartbeat.currentTime = 0;
-}, false);
-
-resetBtn.addEventListener('mouseover', function() {
-    heartbeat.play();
-}, false);
-
-resetBtn.addEventListener('mouseleave', function() {
-  heartbeat.pause();
-  heartbeat.currentTime = 0;
-}, false);
+$(function(){
+        $('.musical-container img').hover(
+            function() { 
+                $(this).prev()[0].play()
+            },
+            function() { 
+                $(this).prev()[0].pause()
+            }
+        )
+    })
